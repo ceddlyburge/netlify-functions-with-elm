4dc9e8e4b60d061b5d5ef0fb2ce6ab856741236f
ceddlyburge <ceddlyburge@gmail.com>
2019-08-27 17:27:17 +0100

    Update Main.elm to call the function and display the results

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,22 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
+import Html exposing (Html, text, div, h1, img, p)
 import Html.Attributes exposing (src)
-
+import RemoteData exposing (WebData)
+import Http
+import Json.Decode exposing (..)
 
 ---- MODEL ----
 
 
-type alias Model =
-    {}
+type alias Model = {
+    tokenAndUrl: WebData TokenAndUrl }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( Model RemoteData.NotAsked, callFunction)
 
 
 
@@ -23,23 +25,51 @@ init =
 
 type Msg
     = NoOp
+    | FunctionResponse (WebData TokenAndUrl)
 
+type alias TokenAndUrl = {
+    token: String,
+    url: String }
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        FunctionResponse tokenAndUrl ->
+            ( { model | tokenAndUrl = tokenAndUrl }, Cmd.none)
+        _ ->
+            ( model, Cmd.none )
 
 
+callFunction: Cmd Msg
+callFunction =
+    Http.get { 
+        expect = Http.expectJson (RemoteData.fromResult >> FunctionResponse) decodeTokenAndUrl,
+        url = ".netlify/functions/call-api" 
+    }
+
+decodeTokenAndUrl : Decoder TokenAndUrl
+decodeTokenAndUrl =
+    Json.Decode.map2 TokenAndUrl
+        (field "api-token" Json.Decode.string)
+        (field "api-url" Json.Decode.string)
 
 ---- VIEW ----
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
-        ]
+    case model.tokenAndUrl of
+        RemoteData.NotAsked -> 
+            text "Initialising."
+
+        RemoteData.Loading -> 
+            text "Loading."
+            
+        RemoteData.Failure err -> 
+            text "Error."
+
+        RemoteData.Success tokenAndUrl -> 
+            text ("Api Token: " ++ tokenAndUrl.token ++ ", Api Url: " ++ tokenAndUrl.url)
 
 
 
